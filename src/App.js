24ba569f2d8b7e9aa17d78cee10e762d852ba3e1
Ochir Erkhembayar <ochir_erkhembayar@yahoo.com>
2022-06-24 17:15:50 +0100

    responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Route } from "react-router-dom";
 
-import Navbar from "./components/layout/Navbar";
+// import Navbar from "./components/layout/Navbar";
+import Navbar2 from "./components/layout/Navbar2";
 // import Footer from "./components/layout/Footer";
 import Banner from "./components/UI/Banner";
 import Matches from "./components/pages/Matches";
@@ -12,7 +13,8 @@ function App() {
 
   return (
     <React.Fragment>
-      <Navbar />
+      {/* <Navbar /> */}
+      <Navbar2 />
         <Route exact path="/">
           <Banner/>
         </Route>
